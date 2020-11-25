5a02c27082a48426062b7c0fb44feb945bff534e
NaonLee <giraffe1254@gmail.com>
2020-11-25 21:59:52 +0900

    DB 저장/화면에 뿌리기

--- src/main/java/com/naon/practice/springboot/service/posts/PostsService.java
+++ src/main/java/com/naon/practice/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.naon.practice.springboot.service.posts;
 
 import com.naon.practice.springboot.domain.posts.Posts;
 import com.naon.practice.springboot.domain.posts.PostsRepository;
+import com.naon.practice.springboot.web.dto.PostsListResponseDto;
 import com.naon.practice.springboot.web.dto.PostsResponseDto;
 import com.naon.practice.springboot.web.dto.PostsSaveRequestDto;
 import com.naon.practice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("There is no such post. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
