f2e39b02792a99f1b9bf2c7bab8e3afefe527dac
NaonLee <giraffe1254@gmail.com>
2020-11-26 22:12:43 +0900

    Spring Security Applied

--- src/main/java/com/naon/practice/springboot/Application.java
+++ src/main/java/com/naon/practice/springboot/Application.java
@@ -3,9 +3,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-@EnableJpaAuditing          //JPA Auditing 활성화
 
+//@EnableJpaAuditing          //JPA Auditing 활성화->삭제. SpringBootApplication과 분리
+@SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
